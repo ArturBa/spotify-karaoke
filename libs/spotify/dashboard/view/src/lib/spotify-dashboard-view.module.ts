6186bf516049cbc28146df89b850efacb33f856d
Artur Bauer <bauerart1@gmail.com>
2021-04-23 09:31:10 +0200

    feat(spotify): :sparkles: Add basic components for new views

--- libs/spotify/dashboard/view/src/lib/spotify-dashboard-view.module.ts
+++ libs/spotify/dashboard/view/src/lib/spotify-dashboard-view.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [DashboardComponent],
+  exports: [DashboardComponent],
 })
 export class SpotifyDashboardViewModule {}
--- libs/spotify/homepage/view/src/lib/spotify-homepage-view.module.ts
+++ libs/spotify/homepage/view/src/lib/spotify-homepage-view.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SpotifyDashboardViewModule } from '@artur-ba/spotify/dashboard/view';
+import { SpotifyPlayerViewModule } from '@artur-ba/spotify/player/view';
+import { SpotifySidebarViewModule } from '@artur-ba/spotify/sidebar/view';
 import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SpotifyDashboardViewModule,
+    SpotifyPlayerViewModule,
+    SpotifySidebarViewModule,
+  ],
   declarations: [HomepageComponent],
   exports: [HomepageComponent],
 })
