322d0a3601e5e9d1cdddaa440901a7e20be764e2
Artur Bauer <bauerart1@gmail.com>
2021-04-24 21:04:32 +0200

    fix(spotify): :lipstick: Remove remaining tailwindcss

--- libs/web/spotify/shared/view/src/lib/song-row/song-row.component.ts
+++ libs/web/spotify/shared/view/src/lib/song-row/song-row.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { TrackHelper } from '@artur-ba/web/spotify/shared/helper';
 @Component({
-  selector: 'artur-ba-song-row',
+  selector: '[artur-ba-song-row]',
   templateUrl: './song-row.component.html',
   styleUrls: ['./song-row.component.scss'],
 })
