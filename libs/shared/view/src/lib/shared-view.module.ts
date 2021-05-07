6f9cd7b925d3fa1233d2d06e61e78338dbda6924
Artur Bauer <bauerart1@gmail.com>
2021-05-07 12:44:29 +0200

    feat(shared): :lipstick: Add gh icon
    Signed-off-by: Artur Bauer <bauerart1@gmail.com>

--- libs/shared/view/src/lib/shared-view.module.ts
+++ libs/shared/view/src/lib/shared-view.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 import { AppInfoDialogComponent } from './app-info/app-info.component';
 import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatDialogModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+  ],
   declarations: [CookieBannerComponent, AppInfoDialogComponent],
   exports: [CookieBannerComponent, AppInfoDialogComponent],
 })
