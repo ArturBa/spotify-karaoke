e13861a265f645baf9178a182bdcf913543bd599
Artur Bauer <bauerart1@gmail.com>
2021-06-24 09:07:46 +0200

    fix: linting

--- libs/shared/service/src/lib/country.service.spec.ts
+++ libs/shared/service/src/lib/country.service.spec.ts
@@ -1,8 +1,8 @@
+import { getTestBed, TestBed } from '@angular/core/testing';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { getTestBed, TestBed } from '@angular/core/testing';
 
 import { CountryService } from './country.service';
 
