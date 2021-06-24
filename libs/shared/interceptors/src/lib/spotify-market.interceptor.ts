bdd78ef0bc0ccb52d10c4f162ff40e810a5e51f6
Artur Bauer <bauerart1@gmail.com>
2021-06-24 16:26:03 +0200

    feat(shared): :sparkles: Add spotify market interceptor

---
+++ libs/shared/interceptors/src/lib/spotify-market.interceptor.ts
@@ -0,0 +1,41 @@
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+
+import { CountryService } from '@artur-ba/shared/service';
+
+@Injectable()
+export class SpotifyMarketInterceptor implements HttpInterceptor {
+  static readonly spotifyApiUrl = 'https://api.spotify.com/';
+  protected readonly spotifyAPIRegex = new RegExp(
+    `^${SpotifyMarketInterceptor.spotifyApiUrl}`
+  );
+
+  constructor(protected readonly countryService: CountryService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (!this.spotifyAPIRegex.test(request.url)) {
+      return next.handle(request);
+    }
+    return from(this.addMarketParamToRequest(request, next));
+  }
+
+  protected async addMarketParamToRequest(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Promise<HttpEvent<unknown>> {
+    const userCountry = await this.countryService.getUserCountry();
+    request = request.clone({
+      params: request.params.append('market', userCountry),
+    });
+    return next.handle(request).toPromise();
+  }
+}
