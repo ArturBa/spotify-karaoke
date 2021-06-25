62fd686a59a892fc055bb6049d3e7c46dfda705e
Artur Bauer <bauerart1@gmail.com>
2021-06-25 11:50:13 +0200

    remove default mode

--- libs/web/spotify/shared/view/.storybook/preview.js
+++ libs/web/spotify/shared/view/.storybook/preview.js
@@ -3,7 +3,6 @@ import { themes } from '@storybook/theming';
 
 export const parameters = {
   darkMode: {
-    current: 'dark',
     dark: { ...themes.dark, appBg: '#171717' },
     darkClass: 'darkMode',
     light: { ...themes.normal, appBg: '#f0f0f0' },
