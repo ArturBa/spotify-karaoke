f98c8684d5f14052cd0e015b38402fa54e12ef77
Artur Bauer <bauerart1@gmail.com>
2021-07-20 19:22:00 +0200

    feat: :memo: Add readme on welcome page

---
+++ apps/spotify/src/app/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { LoginComponent } from './login/login.component';
+import { ReadmeComponent } from './readme/readme.component';
+
+const exports = [LoginComponent];
+
+@NgModule({
+  declarations: [...exports, ReadmeComponent],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  exports,
+})
+export class LoginModule {}
