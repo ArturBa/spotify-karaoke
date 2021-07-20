c4abc9888c287164d12b60adaafebd7a0795f2fa
Artur Bauer <bauerart1@gmail.com>
2021-07-20 19:52:47 +0200

    fix linting

--- apps/spotify/src/app/login/login.module.ts
+++ apps/spotify/src/app/login/login.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
 
 import { LoginComponent } from './login/login.component';
 import { ReadmeComponent } from './readme/readme.component';
