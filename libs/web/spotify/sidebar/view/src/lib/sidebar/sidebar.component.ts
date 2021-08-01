6cae0243649bb60298dd971ce6b25801800b774c
Artur Bauer <bauerart1@gmail.com>
2021-08-01 18:18:47 +0200

    style: :lipstick: Add a narrow sidebar on lower resolution

--- libs/web/spotify/sidebar/view/src/lib/sidebar/sidebar.component.ts
+++ libs/web/spotify/sidebar/view/src/lib/sidebar/sidebar.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'artur-ba-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
+  host: {
+    '(mouseenter)': 'onMouseEnter()',
+    '(mouseleave)': 'onMouseLeave()',
+  },
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  protected el: HTMLElement;
+  protected noHoverClassName = 'sidebar--no-hover';
+
+  constructor(protected readonly elementRef: ElementRef) {
+    this.el = elementRef.nativeElement;
+    this.initNoHover();
+  }
+
+  onMouseEnter() {
+    this.setHover();
+  }
+  onMouseLeave() {
+    this.unsetHover();
+  }
+
+  protected initNoHover() {
+    this.unsetHover();
+  }
+
+  protected unsetHover() {
+    this.el.classList.add(this.noHoverClassName);
+  }
+
+  protected setHover() {
+    this.el.classList.remove(this.noHoverClassName);
+  }
+}
