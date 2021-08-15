1aede5808f845cb9d8ec3985f6985465bca806c4
Artur Bauer <bauerart1@gmail.com>
2021-08-15 16:06:30 +0200

    feat(spotify): :sparkles: Add non premium user message

---
+++ libs/web/spotify/player/view/src/lib/player-free/player-free.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'artur-ba-player-free',
+  templateUrl: './player-free.component.html',
+  styleUrls: ['./player-free.component.scss'],
+})
+export class PlayerFreeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
