a7f9b287684f93c3976b3e91b537b43f856a9ea1
Artur Bauer <bauerart1@gmail.com>
2021-08-15 18:02:40 +0200

    fix lint

--- libs/web/spotify/player/view/src/lib/player-free/player-free.component.ts
+++ libs/web/spotify/player/view/src/lib/player-free/player-free.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'artur-ba-player-free',
   templateUrl: './player-free.component.html',
   styleUrls: ['./player-free.component.scss'],
 })
-export class PlayerFreeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class PlayerFreeComponent {}
