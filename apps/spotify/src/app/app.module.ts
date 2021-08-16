e9435000189afb455f617cc76a5fb3a8fee7b518
Artur Bauer <bauerart1@gmail.com>
2021-08-16 19:38:58 +0200

    sort

--- apps/spotify/src/app/app.module.ts
+++ apps/spotify/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 import {
   NgxGoogleAnalyticsModule,
   NgxGoogleAnalyticsRouterModule,
 } from 'ngx-google-analytics';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 import {
   ProxyInterceptor,
